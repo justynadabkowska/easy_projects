2fc6f9968131533b0ed187810c5dd3a7fe5cc551
Justyna Dąbkowska <justa2901@interia.pl>
2023-05-10 19:18:23 +0200

    Next project - in process

---
+++ password_managers.py
@@ -0,0 +1,17 @@
+pwd = input("What is the master password?")
+def view():
+    pass
+
+def add():
+    pass
+while True:
+    mode = input("Would you like to add a new password or view existing ones? (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+    elif mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
