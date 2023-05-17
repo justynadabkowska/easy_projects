66f2219afd1ea823a804307f9b51078fb9e3c8d8
Justyna Dąbkowska <justa2901@interia.pl>
2023-05-17 20:15:15 +0200

    Next project - in process

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -12,3 +12,27 @@ def print_board(board):
 
 
 print_board(board)
+
+def quit(user_input):
+    if user_input.lower() == "q":
+        print("Thanks for playing")
+        return True
+    else: return False
+
+def check_input(user_input):
+    isnum(user_input)
+
+def isnum(user_input):
+    if not user_input.isnumeric():
+        print("This is not a valid number")
+        return False
+    else:
+        return True
+
+while True:
+    user_input = input("Please enter a position 1 through 9 or enter 'q' to quit: ")
+    if quit(user_input):
+        break
+    if not check_input(user_input):
+        print("Please try again.")
+        continue
