efb919dfe538cf7bdb294b648c946423049446ff
Justyna Dąbkowska <justa2901@interia.pl>
2023-05-17 21:02:35 +0200

    Next project - in process

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -4,14 +4,13 @@ board = [
     ["-", "-", "-"]
 ]
 
+user = True
+
 def print_board(board):
     for row in board:
         for slot in row:
             print(f"{slot}", end=" ")
         print()
-
-
-print_board(board)
 
 def quit(user_input):
     if user_input.lower() == "q":
@@ -41,10 +40,46 @@ def bounds(user_input):
     else:
         return True
 
+def istaken(coords, board):
+    row = coords[0]
+    col = coords[1]
+    if board[row][col] != "-":
+        print("This position is already taken.")
+        return True
+    else:
+        return False
+
+def coordinates(user_input):
+    row = int(user_input / 3)
+    col = user_input
+    if col > 2:
+        col = int(col % 3)
+    return(row, col)
+
+def add_to_board(coords, board, active_user):
+    row = coords[0]
+    col = coords[1]
+    board[row][col] = active_user
+
+def current_user(user):
+    if user:
+        return "x"
+    else:
+        return "o"
+
 while True:
+    active_user = current_user(user)
+    print_board(board)
     user_input = input("Please enter a position 1 through 9 or enter 'q' to quit: ")
     if quit(user_input):
         break
     if not check_input(user_input):
         print("Please try again.")
         continue
+    user_input = int(user_input) - 1
+    coords = coordinates(user_input)
+    if istaken(coords, board):
+        print("Please try again.")
+        continue
+    add_to_board(coords, board, active_user)
+    user = not user
